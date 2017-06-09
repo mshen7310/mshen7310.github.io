df8bc794433fc69f21f2c74574e51bf2dc3a69b9
michael shen <michaelshen@michaels-MacBook-Pro.local>
2017-06-09 18:35:49 +0800

    4

--- index.js
+++ index.js
@@ -4,3 +4,23 @@ self.addEventListener('install', function(e) {
 self.addEventListener('activate', function(e) {
   	console.log('Activate Event:', e);
 });
+self.addEventListener('fetch', function(event) {
+	event.respondWith(
+	    fetch(event.request).then(function(response) {
+	        caches.open(cacheName).then(function(cache) {
+	            if(response.status >= 500) {
+	                cache.match(event.request).
+	                then(function(response) {
+	                    return response;
+	                }).catch(function() {
+	                    return response;
+					}); 
+	            } else {
+                    cache.put(event.request,
+                    response.clone());
+                    return response;
+				} 
+			});
+		});
+	);
+});	                    
